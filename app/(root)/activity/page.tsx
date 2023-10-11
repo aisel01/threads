3d4c34f3560de67438194e94488d09cf7ddbb9d1
Aisel Sher <aisel.goo@gmail.com>
2023-10-12 00:45:18 +0300

    feat: activity page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,11 +1,67 @@
+import { getActivity, getUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
 const Page = async () => {
-    return (
-      <section>
-          <h1 className="head-text mb-10">
-              Activity
-          </h1>
-      </section>
-    )
-  }
+    const user = await currentUser();
+
+    if (!user) {
+        return null;
+    }
   
-  export default Page;
+    const userInfo = await getUser(user.id);
+  
+    if (!userInfo?.onboarded) {
+        redirect('/onboarding');
+    }
+    
+    const activity = await getActivity(userInfo._id);
+
+    return (
+        <section>
+            <h1 className="head-text mb-10">
+                Activity
+            </h1>
+            <section className="mt-10 flex flex-col gap-5">
+                {activity.length > 0 ? (
+                    <>
+                        {
+                            activity.map(a => {
+                                return (
+                                    <Link
+                                        key={a._id}
+                                        href={`/thread/${a.parentId}`}
+                                        className=""
+                                    >
+                                        <article className='activity-card'>
+                                            <Image
+                                                alt="Profile image"
+                                                src={a.author.image}
+                                                width={20}
+                                                height={20}
+                                                className="rounded-full object-cover" 
+                                                style={{ height: 20 }}
+                                            />
+                                            <p className='!text-small-regular text-light-1'>
+                                                <span className='mr-1 text-primary-500'>
+                                                    {a.author.name}
+                                                </span>{" "}
+                                                replied to your thread
+                                            </p>
+                                        </article>
+                                    </Link>
+                                );
+                            })
+                        }
+                    </>
+                ) : (
+                    <p className="no-result">No activity</p>
+                )}
+            </section>
+    </section>
+    );
+};
+  
+export default Page;
