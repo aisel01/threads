04af455bc9f43844c7e8c543fd92bb54efd9b48f
Aisel Sher <aisel.goo@gmail.com>
2023-10-15 18:19:39 +0300

    style: spacing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,46 +5,46 @@ import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 async function Page() {
-  const user = await currentUser();
+    const user = await currentUser();
 
-  if (!user) {
-      return null;
-  }
+    if (!user) {
+        return null;
+    }
 
-  const userInfo = await getUser(user.id);
+    const userInfo = await getUser(user.id);
 
-  if (!userInfo?.onboarded) {
-      redirect('/onboarding');
-  }
-  
-  const { threads, hasNext } = await getThreads();
+    if (!userInfo?.onboarded) {
+        redirect('/onboarding');
+    }
+    
+    const { threads, hasNext } = await getThreads();
 
-  return (
-    <>
-      <h1 className="head-text text-left">Home</h1>
-      <section className='mt-9 flex flex-col gap-10'>
-        {threads.length === 0 ? (
-          <p className='no-result'>No threads found</p>
-        ) : (
-          <>
-            {threads.map(thread => {
-              return (
-                <ThreadCard
-                  key={thread.id}
-                  id={thread.id}
-                  content={thread.text}
-                  author={thread.author}
-                  createdAt={thread.createdAt}
-                  comments={thread.comments}
-                  community={thread.community}
-                />
-              );
-            })}
-          </>
-        )}
-      </section>
-    </>
-  )
+    return (
+        <>
+        <h1 className="head-text text-left">Home</h1>
+        <section className='mt-9 flex flex-col gap-10'>
+            {threads.length === 0 ? (
+            <p className='no-result'>No threads found</p>
+            ) : (
+            <>
+                {threads.map(thread => {
+                return (
+                    <ThreadCard
+                    key={thread.id}
+                    id={thread.id}
+                    content={thread.text}
+                    author={thread.author}
+                    createdAt={thread.createdAt}
+                    comments={thread.comments}
+                    community={thread.community}
+                    />
+                );
+                })}
+            </>
+            )}
+        </section>
+        </>
+    );
 }
 
 export default Page;
