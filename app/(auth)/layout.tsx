4bee922b5cf20493b2528311f42cfe00d591086d
Aisel Sher <aisel.goo@gmail.com>
2023-10-25 16:58:12 +0300

    fix: auth layout fixes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <ClerkProvider>
             <html lang="en">
-                <body className={`${inter.className}`}>
+                <body className={`${inter.className} bg-secondary`}>
                     <ThemeProvider
                         attribute="class"
                         defaultTheme="dark"
