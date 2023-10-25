7fe60eeb4ddd4182313e63369d0dbcde2142a364
Aisel Sher <aisel.goo@gmail.com>
2023-10-25 16:17:45 +0300

    fix(onboarding): fix empty page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,34 +1,43 @@
 import AccountProfile from '@/components/forms/AccountProfile';
 import { getCurrentUser } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 
 async function Page() {
-    const userInfo = await getCurrentUser();
+    const user = await currentUser();
 
-    if (!userInfo) {
+    if (!user) {
         return null;
     }
+
+    const userInfo = await getCurrentUser();
 
     if (userInfo?.onboarded) {
         redirect('/');
     }
 
-    const userData = {
-        clerkId: userInfo?.clerkId,
-        id: userInfo?.id,
-        username: userInfo?.username || '',
-        name: userInfo?.name || '',
-        bio: userInfo?.bio || '',
-        image: userInfo?.image,
+    const userData = userInfo ?  {
+        clerkId: userInfo.clerkId,
+        id: userInfo.id,
+        username: userInfo.username,
+        name: userInfo.name,
+        bio: userInfo.bio,
+        image: userInfo.image,
+    } : {
+        clerkId: user.id,
+        username: user.username || '',
+        name: '',
+        bio: '',
+        image: user.imageUrl,
     };
 
     return (
-        <main className="mx-auto flex max-w-3xl flex-col justify-center px-10 py-20">
+        <main className="mx-auto flex max-w-3xl flex-col justify-center px-10 py-20 bg-secondary">
             <h1 className="head-text">Onboarding</h1>
             <p className="mt-3 text-base-regular">
                 Complete your profile now to use Threads
             </p>
-            <section className="mt-9 p-10">
+            <section className="mt-10">
                 <AccountProfile
                     user={userData}
                     btnTitle="Continue"
--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -19,13 +19,15 @@ const Page = async () => {
         likes,
     } = await getActivity(userInfo.id);
 
+    const hasLikes = likes.some(thread => thread.likes.length > 0);
+
     return (
         <section>
             <h1 className="head-text mb-10">
                 Activity
             </h1>
             <section className="mt-10 flex flex-col gap-10">
-                {replies.length > 0 || likes.length > 0 ? (
+                {replies.length > 0 || hasLikes ? (
                     <>
                         {
                             replies.map(a => {
